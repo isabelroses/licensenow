1f08df995d399c1e03015a17e6e60ecd1efbd7ba
isabel <isabel@isabelroses.com>
2024-03-21 19:52:20 +0000

    chore(nix): update vers

--- default.nix
+++ default.nix
@@ -4,12 +4,10 @@
 }:
 buildGoModule {
   pname = "licencenow";
-  version = "0.0.1";
+  version = "0.1.0";
 
   src = ./.;
 
-  # lib.fakeSha256 should be used to when deps update, but its not working for me so im leaving this here
-  #  sha256-0000000000000000000000000000000000000000000=
   vendorHash = "sha256-aVk0ccByDS4+gs2im4eU6S5daK3OVoRYoBxn3SSgDGw=";
 
   ldflags = ["-s" "-w"];
